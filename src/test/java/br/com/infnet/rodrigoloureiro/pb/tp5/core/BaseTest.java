684a96098d990ea91053159261220eff98094a70
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-21 23:03:14 -0300

    promovendo testes pós deploy com selenium

--- src/test/java/br/com/infnet/rodrigoloureiro/pb/tp5/core/BaseTest.java
+++ src/test/java/br/com/infnet/rodrigoloureiro/pb/tp5/core/BaseTest.java
@@ -1,18 +1,21 @@
 package br.com.infnet.rodrigoloureiro.pb.tp5.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.net.MalformedURLException;
+import java.net.URI;
 import lombok.*;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 @Getter
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws MalformedURLException {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions()
@@ -22,7 +25,9 @@ public class BaseTest {
                 .addArguments("--disable-dev-shm-usage")
                 .addArguments("--disable-gpu");
 
-        this.driver = new ChromeDriver(options);
+        // this.driver = new ChromeDriver(options);
+        URI seleniumHubUri = URI.create("http://localhost:4444/wd/hub");
+        this.driver = new RemoteWebDriver(seleniumHubUri.toURL(), options);
     }
 
     @AfterEach
